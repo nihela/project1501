26b1fa9b049aa2a6a356245e9abca03e94e6a209
nabdelkefi <nihek.abdelkefi@enis.tn>
2024-01-18 19:20:29 +0000

    add a message

--- src/test/java/Tests/SeleniumDemo.java
+++ src/test/java/Tests/SeleniumDemo.java
@@ -24,6 +24,9 @@ public class SeleniumDemo {
     SignInpage.sendkeys_password();
     SignInpage.click_Login_btn();
 
+        System.out.println("Hello, this is my message!");
+        driver.close();
+
 }
 
 }
